1b3bc4cdb40ac66d0465eb4ef507a227afba5f2a
bzq <1634403994@qq.com>
2022-05-28 21:11:53 +0800

    v1.0 init

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -1,6 +1,6 @@
 package com.itheima.reggie;
 
-import com.itheima.reggie.config.LoginConfig;
+
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
